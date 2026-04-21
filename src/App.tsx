2004941cf04ce9cd525db39ad6875a41319e4664
CVlad97 <75902736+CVlad97@users.noreply.github.com>
2026-04-20 23:01:12 -0400

    Simplify public app shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
-import { AuthProvider } from './contexts/AuthContext';
-import { CartProvider } from './contexts/CartContext';
-import { ThemeProvider } from './contexts/ThemeContext';
-import { ToastProvider } from './contexts/ToastContext';
 import { ErrorBoundary } from './components/ErrorBoundary';
 import { PublicHomePage } from './pages/PublicHomePage';
 
 function App() {
   return (
     <ErrorBoundary>
-      <ThemeProvider>
-        <ToastProvider>
-          <AuthProvider>
-            <CartProvider>
-              <PublicHomePage />
-            </CartProvider>
-          </AuthProvider>
-        </ToastProvider>
-      </ThemeProvider>
+      <PublicHomePage />
     </ErrorBoundary>
   );
 }
