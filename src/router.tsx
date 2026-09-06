f28d655e2b830d8577d16e096f983d8923b701ff
CVlad97 <75902736+CVlad97@users.noreply.github.com>
2026-09-06 14:48:27 -0400

    feat(partners): expose partner catalog studio

--- src/router.tsx
+++ src/router.tsx
@@ -52,7 +52,7 @@ const NotFoundPage = lazy(() => import('./pages/new/NotFoundPage'));
 const ContactPage = lazy(() => import('./pages/new/ContactPage'));
 const FeedbackPage = lazy(() => import('./pages/new/FeedbackPage'));
 const CatererSignupPage = lazy(() => import('./pages/new/CatererSignupPage'));
-const PartnerAccessPage = lazy(() => import('./pages/new/PartnerAccessPage'));
+const PartnerAccessPage = lazy(() => import('./pages/new/PartnerAccessPage'));\nconst PartnerCatalogPage = lazy(() => import('./pages/new/PartnerCatalogPage'));
 const PartnerTerminalPage = lazy(() => import('./pages/new/PartnerTerminalPage'));
 const ReviewPage = lazy(() => import('./pages/new/ReviewPage'));
 const JobsPage = lazy(() => import('./pages/Jobs'));
@@ -198,7 +198,7 @@ export function AppRouter() {
                   <Route path="partenaires-plateforme" element={<ProPartnerPlatformPage />} />
                   <Route path="statut-commande" element={<OrderStatusPage />} />
                   <Route path="carte" element={<DiscoveryMapPage />} />
-                  <Route path="partenaire" element={<PartnerAccessPage />} />
+                  <Route path="partenaire" element={<PartnerAccessPage />} />\n                   <Route path="catalogue-partenaire" element={<PartnerCatalogPage />} />
                   <Route path="espace-partenaire" element={<PartnerWrapper />} />
                   <Route path="espace-livreur" element={<PartnerWrapper />} />
                   <Route path="partner-documents" element={<PartnerWrapper />} />
